403444a2a66e309cd536d539851909695e26c8d7
Janecki79 <janecki79@gmail.com>
2025-07-27 18:34:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const memoryRoutes = require('./routes/memoryRoutes');
+const memoryRoutes = require('./routes/memoryRoutes.js');
 
 app.use(express.json());
 app.use("/", memoryRoutes);
